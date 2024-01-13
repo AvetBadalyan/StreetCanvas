358f28baadd17fd7c4a2d71463869bceda77feb2
AvetBadalyan <avetbadalyan@gmail.com>
2024-01-13 21:25:32 +0400

    make main front end pages with forms and validation

--- web/src/App.js
+++ web/src/App.js
@@ -1,7 +1,52 @@
-import "./App.css";
+import React, { useState, useCallback } from "react";
 
-function App() {
-  return <div className="App">app</div>;
-}
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import { AuthContext } from "./shared/context/auth-context";
+import Users from "./user/pages/Users";
+import UserPlaces from "./places/pages/UserPlaces";
+import NewPlace from "./places/pages/NewPlace";
+import UpdatePlace from "./places/pages/UpdatePlace";
+import Auth from "./user/pages/Auth";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+    >
+      <BrowserRouter>
+        <MainNavigation />
+        <main>
+          <Routes>
+            <Route path="/" element={<Users />} />
+            <Route path="/:userId/places" element={<UserPlaces />} />
+            <Route
+              path="/places/new"
+              element={isLoggedIn ? <NewPlace /> : <Navigate to="/auth" />}
+            />
+            <Route
+              path="/places/:placeId"
+              element={isLoggedIn ? <UpdatePlace /> : <Navigate to="/auth" />}
+            />
+            <Route
+              path="/auth"
+              element={!isLoggedIn ? <Auth /> : <Navigate to="/" />}
+            />
+          </Routes>
+        </main>
+      </BrowserRouter>
+    </AuthContext.Provider>
+  );
+};
 
 export default App;
